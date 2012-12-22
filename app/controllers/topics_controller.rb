cec70d0ebb48a6a2a6c809b64b55dd4720b380e5
Cui Liqiang <cui.liqiang@gmail.com>
2012-12-22 13:24:01 +0800

    add bootstrap in; finish index page with basic styling and edit/delete button; finish edit functionality

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,16 @@
 class TopicsController < ApplicationController
+	def edit
+		@topic = Topic.find(params[:id])
+	end
+
+	def update
+		@topic = Topic.find(params[:id])
+		@topic.update_attributes params[:topic]
+		update_or_create_item
+
+		redirect_to edit_topic_path @topic
+	end
+
 	def index
 		@topics = Topic.all
 	end
@@ -13,5 +25,12 @@ class TopicsController < ApplicationController
 			@topic.items << Item.new(item)
 		end
 		redirect_to :new_topic
+	end
+
+	private 
+	def update_or_create_item
+		params[:items].each do |item|
+			item[:id].nil? ? @topic.items << Item.new(item) : Item.find(item[:id]).update_attributes(item)
+		end
 	end
 end
